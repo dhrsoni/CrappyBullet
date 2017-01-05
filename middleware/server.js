d02547076796b21dfd502c1fc726dad0cc4bfe66
Cameron Spencer <cameron.m.spencer@drexel.edu>
2017-01-05 18:17:19 -0500

    Add single server runnable file.

---
+++ middleware/server.js
@@ -0,0 +1,23 @@
+var io = require('socket.io').listen(1180);
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
+}, function(err){
+  console.log(err);
+});
+
+con.connect(function(err){
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log("connection successful");
+});
+
+require('./socket-interface/server.js')(io);
+require('./mysql-interface/user.js')(con);
+
--- middleware/socket-interface/server.js
+++ middleware/socket-interface/server.js
@@ -1,26 +1,27 @@
-var io = require('socket.io').listen(1180);
-var namespaceMessage = io.of('/messages'); // create a message namespace
+module.exports = function(io) {
+  var namespaceMessage = io.of('/messages'); // create a message namespace
 
-namespaceMessage.on('connection', function(socket) {
-  // TODO: On connection, a room is auto created with the connection id.
-  //    If possible we should should not allow this. Helpful links below
-  //    http://stackoverflow.com/questions/35304622/socket-io-1-0-0-using-the-default-room-vs-a-custom-room-for-user-specific-com
-  //    http://stackoverflow.com/questions/37504319/socket-io-disable-automatical-joining-a-room-identified-by-sockets-id
+  namespaceMessage.on('connection', function(socket) {
+    // TODO: On connection, a room is auto created with the connection id.
+    //    If possible we should should not allow this. Helpful links below
+    //    http://stackoverflow.com/questions/35304622/socket-io-1-0-0-using-the-default-room-vs-a-custom-room-for-user-specific-com
+    //    http://stackoverflow.com/questions/37504319/socket-io-disable-automatical-joining-a-room-identified-by-sockets-id
 
-  socket.on('joinRoom', function(data) {
-    socket.join(data.room);
+    socket.on('joinRoom', function(data) {
+      socket.join(data.room);
 
-    // print all connected rooms
-    //console.log(namespaceMessage.adapter.rooms);
+      // print all connected rooms
+      //console.log(namespaceMessage.adapter.rooms);
+    });
+
+    socket.on('disconnect', function(data) {
+      // TODO: Do something on disconnect
+    });
+
+    socket.on('sendMessage', function(data) {
+      // TODO: Add message to database and if successful then broadcast newMessage
+      socket.broadcast.to(data.room).emit('newMessage', data);
+    });
   });
-
-  socket.on('disconnect', function(data) {
-    // TODO: Do something on disconnect
-  });
-
-  socket.on('sendMessage', function(data) {
-    // TODO: Add message to database and if successful then broadcast newMessage
-    socket.broadcast.to(data.room).emit('newMessage', data);
-  });
-});
+}
 
